2f0382c07c65399201b4eccb148199f3a56c115b
Marko Dujović <dujovicm@gmail.com>
2021-06-12 15:41:18 +0200

    SistemManager - Dodavanje

--- src/projekat/sistem/SistemManager.java
+++ src/projekat/sistem/SistemManager.java
@@ -1,6 +1,8 @@
 package projekat.sistem;
 
 import projekat.Main;
+import projekat.knjiga.Autor;
+import projekat.knjiga.Izdavac;
 import projekat.knjiga.Knjiga;
 import projekat.osoba.Administrator;
 import projekat.osoba.Clan;
@@ -116,11 +118,143 @@ public class SistemManager {
 		}
 
 		a.serialize();
+		adminList.add(a);
 	}
-	private static void addMember() {}
-	private static void addBook() {}
-	private static void addAuthor() {}
-	private static void addPublisher() {}
+	private static void addMember(ArrayList<Clan> clanovi) {
+		Scanner scanner = new Scanner(System.in);
+		Main.cls();
+		System.out.println("Dodavanje novog clana...");
+		Clan c = new Clan();
+
+		System.out.print("Unesite ime: "); c.setIme(scanner.nextLine());
+		System.out.print("Unesite prezime: "); c.setPrezime(scanner.nextLine());
+		System.out.print("Unesite adresu stanovanja: "); c.setAdresa(scanner.nextLine());
+		System.out.print("Unesite broj telefona: "); c.setBrTelefona(scanner.nextLine());
+		System.out.print("Unesite email: "); c.setEmail(scanner.nextLine());
+
+		boolean petlja = true;
+
+		while (petlja) {
+			System.out.println("\n1. Musko\n2. Zensko");
+			System.out.println("Unos: ");
+			switch (scanner.nextInt()) {
+				case 1:
+					petlja = false;
+					c.setPol(EnumPol.MUSKO.getNum());
+					break;
+				case 2:
+					petlja = false;
+					c.setPol(EnumPol.ZENSKO.getNum());
+					break;
+				default:
+					System.out.println("Molimo unesite jednu od dostupnih opcija!");
+			}
+		}
+
+		c.serialize();
+		clanovi.add(c);
+	}
+	private static void addBook(ArrayList<Knjiga> knjige, ArrayList<Autor> autori, ArrayList<Izdavac> izdavaci) {
+		Scanner scanner = new Scanner(System.in);
+		Main.cls();
+		System.out.println("Dodavanje nove knjige...");
+		Knjiga k = new Knjiga();
+		k.setId(k.generateUUID());
+		System.out.println("Unesite ime knjige: ");
+		System.out.println("Unesite ISBN: ");
+		System.out.println("Unesite godinu objavljivanja: ");
+		System.out.println("Unesite izdanje knjige: ");
+		System.out.println("Unesite broj strana: ");
+		System.out.println("Unesite kolicinu: ");
+
+		boolean petlja = true;
+		System.out.println("Autori: ");
+		System.out.println("Odaberite opciju: ");
+		while (petlja) {
+			System.out.println("1. Odaberite postojece autore\n2. Novi autor/i");
+			System.out.println("Unos: ");
+			switch (scanner.nextInt()) {
+				case 1:
+					petlja = false;
+
+					break;
+				case 2:
+					Autor a = new Autor();
+					a.setId(a.generateUUID());
+					System.out.print("Unesite ime autora: "); a.setIme(scanner.nextLine());
+					System.out.print("Unesite prezime autora: "); a.setPrezime(scanner.nextLine());
+
+					k.getAutori().add(a);
+
+					System.out.print("Da li zelite da dodate jos jednog autora? (Y/N): ");
+					if (scanner.nextLine().equalsIgnoreCase("Y")) {
+						continue;
+					} else {
+						petlja = false;
+					}
+					break;
+				default:
+					System.out.println("Molimo unesite jednu od dostupnih opcija!");
+			}
+		}
+
+		petlja = true;
+		System.out.println("Izdavac: ");
+		System.out.println("Odaberite opciju: ");
+		while (petlja) {
+			System.out.println("1. Odaberite postojeceg izvodjaca\n2. Novi izdavac/i");
+			System.out.println("Unos: ");
+			switch (scanner.nextInt()) {
+				case 1:
+					petlja = false;
+
+					break;
+				case 2:
+					petlja = false;
+
+					Izdavac i = new Izdavac();
+					i.setId(i.generateUUID());
+					System.out.print("Unesite naziv izdavaca: "); i.setImeIzdavaca(scanner.nextLine());
+					System.out.print("Unesite zemlju izdavaca: "); i.setZemljaPorekla(scanner.nextLine());
+
+					k.setIzdavac(i);
+					izdavaci.add(i);
+
+					break;
+				default:
+					System.out.println("Molimo unesite jednu od dostupnih opcija!");
+			}
+		}
+
+		k.serialize();
+		knjige.add(k);
+	}
+	private static void addAuthor(ArrayList<Autor> autori) {
+		Scanner scanner = new Scanner(System.in);
+		Main.cls();
+		System.out.println("Dodavanje novog autora...");
+		Autor a = new Autor();
+
+		a.setId(a.generateUUID());
+		System.out.println("Unesite ime autora: "); a.setIme(scanner.nextLine());
+		System.out.println("Unesite prezime autora: "); a.setPrezime(scanner.nextLine());
+
+		a.serialize();
+		autori.add(a);
+	}
+	private static void addPublisher(ArrayList<Izdavac> izdavaci) {
+		Scanner scanner = new Scanner(System.in);
+		Main.cls();
+		System.out.println("Dodavanje novog autora...");
+		Izdavac i = new Izdavac();
+
+		i.setId(i.generateUUID());
+		System.out.println("Unesite naziv izdavaca: "); i.setImeIzdavaca(scanner.nextLine());
+		System.out.println("Unesite zemlju: "); i.setZemljaPorekla(scanner.nextLine());
+
+		i.serialize();
+		izdavaci.add(i);
+	}
 
 	private static void editAdmin(ArrayList<Administrator> adminList) {}
 	private static void editMember() {}
